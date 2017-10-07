01a53b94775719c99bd80740b2786fe227a1c0f5
SASANGA <chathumal.sasanga02@gmail.com>
2017-10-07 23:03:24 +0530

    create separate file for static strings and add bootstrap for start styling.

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { StaticStrings } from '../../statics/static-strings';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
   styleUrls: ['./index.component.css']
 })
-export class IndexComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class IndexComponent{
+  private strings = StaticStrings;
 
 }
---
+++ src/statics/static-strings.ts
@@ -0,0 +1,11 @@
+/**
+ * Created by SASANGA on 10/5/2017.
+ */
+export var StaticStrings = {
+  navBarBrand: 'E-way',
+  navBarSections: {
+    firstSection: 'Friends',
+    secondSection: 'Chats',
+    thirdSection: 'settings'
+  }
+};
